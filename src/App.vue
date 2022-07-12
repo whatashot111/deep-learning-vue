e6982c33823b5b6d87db0423835591a2180c44e3
whatashot111 <harsh26011995@gmail.com>
2022-07-12 22:31:22 +0530

    added deep learning code

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div>
+      <div v-if="browser">You are using Browser</div>
+      <div v-else-if="android">You are using Android</div>
+    </div>
+    <div>
+      <button @click="openApp">Open Instagram</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { isBrowser, isAndroid } from "mobile-device-detect";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  data() {
+    return {
+      browser: isBrowser,
+      android: isAndroid,
+    };
+  },
+  methods: {
+    openApp() {
+      if (this.android) {
+        const url =
+          "intent://instagram.com/#Intent;scheme=https;package=com.instagram.android;end";
+
+        window.location.replace(url);
+      } else if (this.browser) {
+        window.location.replace("https://instagram.com");
+      }
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  font: inherit;
+  background: cyan;
+  padding: 8px;
+  border-radius: 8px;
+  margin-top: 10px;
+  cursor: pointer;
 }
 </style>
